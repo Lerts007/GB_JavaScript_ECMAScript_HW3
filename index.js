50a369785188f93343bcb211ec31d676791840fb
Lerts <oddff@mail.ru>
2023-10-02 21:37:53 +0300

    Completed task 1-2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 // Свойство name (имя) - строка, имя сотрудника.
 // Метод displayInfo() - выводит информацию о сотруднике (имя).
+
 // Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
 
 // Свойство department (отдел) - строка, отдел, в котором работает менеджер.
@@ -26,6 +27,34 @@
 console.log('--------------------------------------------------------------------');
 console.log('Задание 1');
 console.log('--------------------------------------------------------------------');
+
+class Employee {
+  constructor(name) {
+    this.name = name;
+  }
+
+  displayInfo() {
+    console.log(`Name: ${this.name}`);
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, deportment) {
+    super(name);
+    this.deportment = deportment;
+  }
+
+  displayInfo() {
+    console.log(`Name: ${this.name}`);
+    console.log(`Department: ${this.deportment}`);
+  }
+}
+
+const employee = new Employee('John Smith');
+employee.displayInfo();
+
+const manager = new Manager('Jane Doe', 'Sales');
+manager.displayInfo();
 
 // --------------------------------------------------------------------
 // Задание 2 "Управление списком заказов"
@@ -60,5 +89,43 @@ console.log('-------------------------------------------------------------------
 console.log('--------------------------------------------------------------------');
 console.log('Задание 2');
 console.log('--------------------------------------------------------------------');
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+class Order {
+  constructor(orderNumber) {
+    this.orderNumber = orderNumber;
+    this.productsArr = [];
+    this.totalCost = 0;
+  }
+
+  addProduct(product) {
+    this.productsArr.push(product);
+  }
+
+  getTotalPrice() {
+    this.totalCost = 0;
+    this.productsArr.forEach((el) => {
+      this.totalCost += el.price;
+    });
+
+    return `Вывод: ${this.totalCost}`;
+  }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product('Phone', 500);
+order.addProduct(product1);
+
+const product2 = new Product('Headphones', 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600
 
 // --------------------------------------------------------------------
